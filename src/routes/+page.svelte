780a2cd4e10ceea3050c88586e96a979af70c06b
Emmanuel Neuenhaus <emanuelneuenhaus@hotmail.com>
2025-07-16 22:01:43 +0200

    settup svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,48 @@
+
+
+<script>
+
+</script>
+
+
+<main>
+
+    <div class="mainComponent">
+        hallo
+
+        <button class="storyBtn">
+            <a   class="storyBtn" href="/story">Story</a>
+        </button>
+    </div>
+
+</main>
+
+
+<style>
+main{
+  background: #aba2a2;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;   
+  justify-content: center; 
+  padding: 0.4%;
+  box-sizing: border-box;  
+}
+
+.mainComponent {
+  background-color: #fff;
+  width: 100%;
+  height: 100%;
+  margin: auto;
+  
+}
+
+.storyBtn{
+    text-decoration: none;
+    color: black;
+    
+}
+
+    
+</style>
